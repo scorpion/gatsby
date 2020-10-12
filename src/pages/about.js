3ba690e99923a61bcca03014cdf25b3ac9d65afd
Brett Wilcox <brett@scorpion.io>
2020-10-12 02:08:18 -0500

    Initialize

---
+++ src/pages/about.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function Home() {
+  return <div>This is the about page.</div>
+}
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function Home() {
+  return <div>Wilcox</div>
+}
